b3fff5b2c91785a92ace62faa7ca223097bf6f5c
Fred <synapsefred@hotmail.fr>
2020-07-15 13:50:01 +0200

    commit

--- Archer.php
+++ Archer.php
@@ -2,5 +2,16 @@
 
 class Archer extends Character
 {
-    
+    public $power;
+
+    public function __construct($name, $cri_instance, $pouv_instance)
+    {
+        parent:: __construct($name, $cri_instance,);
+        $this->power = $pouv_instance;
+    }
+
+    public function rangeAttak($target)
+    {
+        $target->hp -= $this->power;
+    }
 }
